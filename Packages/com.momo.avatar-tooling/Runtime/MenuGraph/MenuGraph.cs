3df9b503a46876b07a93e8eb7ac0eff8ae8b3c52
Momo <moharryg@gmail.com>
2026-03-12 18:25:24 +0000

    Update MenuGraph.cs

--- Packages/com.momo.avatar-tooling/Runtime/MenuGraph/MenuGraph.cs
+++ Packages/com.momo.avatar-tooling/Runtime/MenuGraph/MenuGraph.cs
@@ -11,11 +11,13 @@ namespace MomoVRChatTools
         private VRCAvatarDescriptor avatarDescriptor;
         public VRCAvatarDescriptor AvatarDescriptor { get { return avatarDescriptor; } }
 
+        // Storing the Parameters and Menus here so they can be edited without touching the scriptableObjects
         // This is how to get the max number of VRCExpressionsMenu.Control there can be in any one array.
         //VRCExpressionsMenu.MAX_CONTROLS;
-
-        public List<VRCExpressionParameters.Parameter> avatarParameters = new List<VRCExpressionParameters.Parameter>();
-        public Dictionary<string, List<VRCExpressionsMenu.Control>> avatarMenus = new Dictionary<string, List<VRCExpressionsMenu.Control>>();
+        private List<VRCExpressionParameters.Parameter> avatarParameters = new List<VRCExpressionParameters.Parameter>();
+        private Dictionary<string, List<VRCExpressionsMenu.Control>> avatarMenus = new Dictionary<string, List<VRCExpressionsMenu.Control>>();
+        public List<VRCExpressionParameters.Parameter> AvatarParameters {  get { return avatarParameters; } }
+        public Dictionary<string, List<VRCExpressionsMenu.Control>> AvatarMenus {  get { return avatarMenus; } }
 
         private void Reset()
         {
